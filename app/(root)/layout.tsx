ee9e0c4bf55ddf046a24ca29dbec79d7a368b37c
Mohammad Zgoul <moeazgoul@gmail.com>
2025-03-29 02:30:44 +0300

    activatied github authenatication
    updated tailwindcss config in globals.css

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+function layout({ children }: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <main className="font-work-sans">
+      <Navbar />
+      {children}
+    </main>
+  );
+}
+
+export default layout;
---
+++ app/components/Navbar.tsx
@@ -0,0 +1,54 @@
+import { auth, signIn, signOut } from "@/auth";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+async function Navbar() {
+  const session = await auth();
+  return (
+    <div className="px-5 py-3 bg-white shadow-sm font-work-sans">
+      <nav className="flex justify-between items-center">
+        <Link href="/">
+          <Image src="/logo.png" alt="logo" width={144} height={30} />
+        </Link>
+
+        <div className="flex items-center gap-5 text-black">
+          {session && session.user ? (
+            <>
+              <Link href="/startup/create">
+                <span>Create</span>
+              </Link>
+              <form
+                action={async () => {
+                  "use server";
+                  await signOut({ redirectTo: "/" });
+                }}
+              >
+                <button type="submit" className="cursor-pointer">
+                  Logout
+                </button>
+              </form>
+
+              <Link href={`/user/${session.user?.id}`}>
+                <span>{session.user?.name}</span>
+              </Link>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+                await signIn("github");
+              }}
+            >
+              <button type="submit" className="cursor-pointer">
+                Login
+              </button>
+            </form>
+          )}
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
