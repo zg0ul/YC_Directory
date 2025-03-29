3450f96daee9a06d72352b357dac643327f50ed4
Mohammad Zgoul <moeazgoul@gmail.com>
2025-03-29 22:35:23 +0300

    fixed tailwind and shadcn css

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,15 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1 className="text-2xl">Home</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br /> Connect With Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
+        </p>
+      </section>
     </>
   );
 }
